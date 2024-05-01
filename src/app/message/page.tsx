28b227d9ac39bef20938964eaf23e92b7b663029
pulkit-singhall <singhal.pulkit667@gmail.com>
2024-05-02 00:36:55 +0530

    send message frontend page

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -1,7 +1,107 @@
+'use client'
+import { useRouter, useSearchParams } from "next/navigation"
+import axios from "axios"
+import { useEffect, useState, useRef } from "react"
+import NavBar from "@/components/navbar"
+
 export default function Message() {
+    let router = useRouter()
+    let searchParams = useSearchParams()
+
+    // useRef
+    let contentRef = useRef(null)
+
+    // useState
+    let [username, setUsername] = useState('')
+    let [errorMessage, setErrorMessage] = useState('')
+    let [message, setMessage] = useState('')
+
+    useEffect(() => {
+        let userName = searchParams.get('username')
+        if (userName) {
+            setUsername(userName)
+        }
+    }, [])
+    
+    async function onSignup() {
+        router.push('/signup')
+    }
+
+    async function onLogin() {
+        router.push('/login')
+    }
+
+    function resetContent() {
+        contentRef.current.value = ''
+        setMessage('')
+        setErrorMessage('')
+    }
+
+    async function sendMessage() {
+        if (message.length > 300) {
+            setErrorMessage('content should not be more than 300 characters')
+            return
+        }
+        else if (message.length < 5) {
+            setErrorMessage('content should be atleast 5 characters')
+            return
+        }
+        axios.post('/api/messages/send', { content: message })
+            .then((res) => {
+                const data = res.data
+                if (data.success) {
+                    
+                }
+                else {
+                    alert(`Error in sending the message : ${data.message}`)
+                }
+            })
+            .catch((err) => {
+                alert(`Error in sending the message : ${err.message}`)
+            })
+    }
+
     return (
-        <>
-            <h1>message page</h1>
-        </>
+        <div className="flex flex-col items-center justify-center">
+            <NavBar
+                onLogin={onLogin}
+                onSignup={onSignup}
+            />
+            <div
+                className="flex flex-col items-start justify-evenly p-3
+                mt-8 mb-5">
+                <p className="text-black text-1xl p-1 m-2">
+                    Send anonymous message to <b>{username}</b>
+                </p>
+                <textarea
+                    ref={contentRef}
+                    onChange={(e) => {
+                        setMessage(e.target.value)
+                    }}
+                    className="border-black border-2 p-2 rounded-md"
+                    cols={65} rows={10} placeholder="Write something!">
+                </textarea>
+                <p
+                    className="text-red-500 text-sm m-2">
+                    {errorMessage}
+                </p>
+            </div>
+            <div
+                className="flex flex-row items-center justify-center w-screen
+                p-2 mt-5">
+                <button
+                    onClick={resetContent}
+                    className="bg-green-500 text-white p-1 mr-4 rounded-md 
+                    h-9 w-16 hover:bg-green-600">
+                    Reset
+                </button>
+                <button
+                    onClick={sendMessage}
+                    className="text-white p-1 ml-2 rounded-md h-9 w-32
+                    bg-gray-500 hover:bg-gray-600">
+                    Send Message
+                </button>
+            </div>
+        </div>
     )
 }
--- src/schema/message.schema.ts
+++ src/schema/message.schema.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
 const messageContentSchema = z.object({
-    content: z.string().min(20, {message: "content should be atleast 20 characters"}).max(300, {message: "content should not be more than 300 characters"})
+    content: z.string()
+        .min(5, { message: "content should be atleast 5 characters" })
+        .max(300, { message: "content should not be more than 300 characters" })
 })
 
 export {
